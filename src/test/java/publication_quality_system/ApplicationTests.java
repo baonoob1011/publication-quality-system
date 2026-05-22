29db633a0f3ad6c1e6d4d900abe06674233df235
baonoob1011 <trandinhbao222@gmail.com>
2026-05-22 13:54:12 +0700

    initial project

---
+++ src/test/java/publication_quality_system/ApplicationTests.java
@@ -0,0 +1,13 @@
+package publication_quality_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
